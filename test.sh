8d0c1b7258f900d73e61d449f35295d402e5f6b0
RyanLI <lyxnb2333@gmail.com>
2022-12-05 00:03:30 +0800

    asdfa
    asdf

---
+++ test.sh
@@ -0,0 +1,14 @@
+python predict_example.py audio/Billy.00020.au
+python predict_example.py audio/Billy.00021.au
+
+python predict_example.py audio/Peter.00020.au
+python predict_example.py audio/Peter.00021.au
+
+python predict_example.py audio/Jackson.00020.au
+python predict_example.py audio/Jackson.00021.au
+
+python predict_example.py audio/Jelly.00020.au
+python predict_example.py audio/Jelly.00021.au
+
+python predict_example.py audio/Alice.00020.au
+python predict_example.py audio/Alice.00021.au
